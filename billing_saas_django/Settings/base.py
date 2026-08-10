69b08c15b13c8bb59c65906be586a40adc466cde
Ezekiel Kirui <shadikirui0@gmail.com>
2026-08-10 15:29:00 +0300

    log in fix

--- billing_saas_django/Settings/base.py
+++ billing_saas_django/Settings/base.py
@@ -124,6 +124,7 @@ EMAIL_PORT = int(os.getenv("EMAIL_PORT", "587"))
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
 EMAIL_USE_TLS = env_bool("EMAIL_USE_TLS", True)
+EMAIL_TIMEOUT = int(os.getenv("EMAIL_TIMEOUT", "10"))
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER or "no-reply@billing-saas.local")
 ADMIN_NOTIFICATION_EMAILS = env_list("ADMIN_NOTIFICATION_EMAILS")
 
